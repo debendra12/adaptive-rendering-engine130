29aff80c570220cc8c6a78712985c7b81d7d813e
Devendra Pandey <debendrapandey3@gmail.com>
2026-06-13 18:30:21 +0545

    Create src/config/engine.config.ts

---
+++ src/config/engine.config.ts
@@ -0,0 +1,15 @@
+export const EngineConfig = {
+
+  projectName:
+    "Adaptive Rendering Engine",
+
+  version:
+    "1.0.0",
+
+  defaultStrategy:
+    "ISR",
+
+  enableCaching: true,
+
+  enableMetrics: true
+};
