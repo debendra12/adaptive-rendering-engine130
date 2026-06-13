1f4dcd35ed1991f122b6d0d87885512c7193b994
Devendra Pandey <debendrapandey3@gmail.com>
2026-06-13 18:26:01 +0545

    Create src/server/middleware.ts

---
+++ src/server/middleware.ts
@@ -0,0 +1,18 @@
+import {
+  Request,
+  Response,
+  NextFunction
+} from "express";
+
+export function requestLogger(
+  req: Request,
+  res: Response,
+  next: NextFunction
+): void {
+
+  console.log(
+    `[${new Date().toISOString()}] ${req.method} ${req.url}`
+  );
+
+  next();
+}
